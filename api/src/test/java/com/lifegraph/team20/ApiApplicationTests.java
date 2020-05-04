13331f3a76cd0a1907c60e60df5857c1df409edf
hidekishibashi <dev.hidekishibashi@gmail.com>
2020-05-04 12:57:33 +0900

    add : java project

---
+++ api/src/test/java/com/lifegraph/team20/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lifegraph.team20;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
