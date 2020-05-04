dd5d19f682ceaa99bc41cf3eba54992ec8e09c8a
hidekishibashi <dev.hidekishibashi@gmail.com>
2020-05-04 15:51:25 +0900

    add : auth settings

---
+++ api/src/main/java/com/lifegraph/team20/auth/AuthController.java
@@ -0,0 +1,16 @@
+package com.lifegraph.team20.auth;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public ResponseEntity<String> login() {
+		return ResponseEntity.ok("OK");
+	}
+}
