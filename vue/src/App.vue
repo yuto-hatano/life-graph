447440f72f7be791c4bf67b8927a13fd9e1fabd4
kanasekiguchi <kana.sekiguchi@seattleconsulting.co.jp>
2020-05-12 11:21:08 +0900

    vue-routerの修正

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -10,17 +10,6 @@
   </div>
 </template>
 
-<script>
-import Header from './components/Header.vue'
-import Login from './components/Login.vue'
-import  Top from './components/Top.vue'
-import Register from './components/Register.vue'
-import Reference from './components/Reference.vue'
-import Search from './components/Search.vue'
-import Footer from './components/Footer.vue'
-
-</script>
-
 <style>
 #app {
   color: #2c3e50;
