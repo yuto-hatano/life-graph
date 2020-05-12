729ffc94844b2b312de7fabb257395e6be596030
haru-sato <seattle-sato@gmail.com>
2020-05-12 16:29:17 +0900

    ヘッダー実装

---
+++ vue/src/views/Header.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="header">
+    <div id="header-left">
+      <button class="btn_top"
+              href="top"
+      >
+        TOP
+      </button>
+    </div>
+    <div id="header-right">
+      <div id="username">
+        username
+      </div>
+      <button class="btn_logout"
+              href="login"
+      >
+        LOGOUT
+      </button>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  #header{
+    height: 50px;
+    background-color: gray;
+  }
+
+  #header-left{
+    text-align: left;
+    padding-top: 10px;
+    padding-left: 10px
+  }
+
+  #header-right{
+    text-align: right;
+
+  }
+  .btn_top,.btn_logout{
+    width: 100px;
+    height: 30pxx;
+    font-size: 20px;
+  }
+</style>
