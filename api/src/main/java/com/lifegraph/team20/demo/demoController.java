dceda40514c5491b093bb4e4be7bd612efed7236
yuto.hatano <yuto.hatano@seattleconsulting.co.jp>
2020-05-12 16:37:40 +0900

    add:demopackage

---
+++ api/src/main/java/com/lifegraph/team20/demo/demoController.java
@@ -0,0 +1,16 @@
+//package com.lifegraph.team20.demo;
+//
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequestMapping("/auth")
+//public class demoController {
+//
+//  @RequestMapping(value = "/login", method = RequestMethod.GET)
+//  public ResponseEntity<String> login() {
+//    return ResponseEntity.ok("OK");
+//  }
+//}
