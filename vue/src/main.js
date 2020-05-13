aa3539346340fd045e9897c07de60da8095b4341
yutoishida <yuto.ishida@seattleconsulting.co.jp>
2020-05-13 18:31:43 +0900

    【ログイン画面モック作成】20200513

--- vue/src/main.js
+++ vue/src/main.js
@@ -4,10 +4,10 @@ import router from './router'
 import store from './store'
 import './assets/css/reset.css'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faUser, faLock } from '@fortawesome/free-solid-svg-icons'
+import { faEnvelope, faLock } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
-library.add(faUser, faLock)
+library.add(faEnvelope, faLock)
 
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 Vue.config.productionTip = false
---
+++ vue/src/views/Register.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="registerSection">
+    <h1>Life Graph</h1>
+  </div>
+</template>
+
+<style scoped>
+
+h1 {
+  /* font-style: Roboto Slab; */
+  background-color: #e5f3f3;
+  font-size: 40px;
+  padding: 35px 0;
+
+}
+</style>
