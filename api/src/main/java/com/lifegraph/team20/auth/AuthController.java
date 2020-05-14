80888c4624d1a68b8fe09595a5dffa495a9312e4
yuto.hatano <yuto.hatano@seattleconsulting.co.jp>
2020-05-14 09:39:10 +0900

    mod:AuthController

--- api/src/main/java/com/lifegraph/team20/auth/AuthController.java
+++ api/src/main/java/com/lifegraph/team20/auth/AuthController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+//  RestControllerではViewに遷移せず、メソッドの戻り値がそのままレスポンスコンテンツになる。
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
