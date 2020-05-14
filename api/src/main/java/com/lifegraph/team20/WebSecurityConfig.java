b4b89a3705df4176e187cfbfd87e036c856a935a
yuto.hatano <yuto.hatano@seattleconsulting.co.jp>
2020-05-14 23:18:50 +0900

    mod:loginAPI

--- api/src/main/java/com/lifegraph/team20/WebSecurityConfig.java
+++ api/src/main/java/com/lifegraph/team20/WebSecurityConfig.java
@@ -1,15 +1,50 @@
 package com.lifegraph.team20;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+  @Autowired
+  UserDetailsServiceImpl userDetailsService;
+
+  @Autowired
+  private AuthEntryPointJwt unautorizedHandler;
+
+  @Bean
+  public AuthTokenFilter authenticationJwtTokenFilter() {
+    return new AuthTokenFilter();
+  }
+
+  @Override
+       public void configure(AuthenticationManagerBuilder authentificationManagerBuilder) throw Exception {
+               authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+       }
+
+  @Bean
+  @Override
+  public AuthenticationManager authenticationManagerBean() throws Exception {
+    return super.authenticationManagerBean();
+  }
+
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncorder();
+  }
 
   @Override
   public void configure(WebSecurity web) throws Exception {
@@ -19,38 +54,47 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
   @Override
   protected void configure(HttpSecurity http) throws Exception {
-    http.authorizeRequests()
-        //    ログイン画面
-        .antMatchers("/Login")
-        //    全ユーザーアクセス可
-        .permitAll()
-        //    管理画面
-        .antMatchers("/auth/login**")
-        //    ADMIN権限者のみアクセス可能
-        .hasRole("ADMIN")
-        //    全てのURLリクエストは認証されているユーザーしかアクセスできない
-        .anyRequest().authenticated();
-    http.formLogin()
-        //    ログイン画面のURL
-        .loginPage("/Login")
-        //    ログイン処理をするURL→API？
-        .loginProcessingUrl("/signin")
-        .usernameParameter("loginID")
-        .passwordParameter("password")
-        //    ログイン成功時に遷移する画面のURL
-        .defaultSuccessUrl("/Top", false)
-        .permitAll()
-        .and()
-        .logout()
-        //    ログアウトのURL
-        .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-        //    ログアウトにアクセスが来たらセッションIDを削除してログイン画面に遷移
-        .logoutSuccessUrl("/Login")
-        .deleteCookies("JSESSIONID")
-        .invalidateHttpSession(true)
-        .permitAll();
+    http.cors().and().csrf().disable()
+        .exceptionHandling().authenticationEntryPoint(unauthorizedeHandler).and()
+        .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+        .authorizeReqests().antMatchers("/api/auth/**").permitAll()
+        .antMatchers("/api/test/**").permitAll()
+        .anyRequest().autheticated();
+    http.addFilterBefore(autheticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
   }
 }
+//    http.authorizeRequests()
+//        //    ログイン画面
+//        .antMatchers("/Login")
+//        //    全ユーザーアクセス可
+//        .permitAll()
+//        //    管理画面
+//        .antMatchers("/auth/login**")
+//        //    ADMIN権限者のみアクセス可能
+//        .hasRole("ADMIN")
+//        //    全てのURLリクエストは認証されているユーザーしかアクセスできない
+//        .anyRequest().authenticated();
+//    http.formLogin()
+//        //    ログイン画面のURL
+//        .loginPage("/Login")
+//        //    ログイン処理をするURL→API？
+//        .loginProcessingUrl("/signin")
+//        .usernameParameter("loginID")
+//        .passwordParameter("password")
+//        //    ログイン成功時に遷移する画面のURL
+//        .defaultSuccessUrl("/Top", false)
+//        .permitAll()
+//        .and()
+//        .logout()
+//        //    ログアウトのURL
+//        .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+//        //    ログアウトにアクセスが来たらセッションIDを削除してログイン画面に遷移
+//        .logoutSuccessUrl("/Login")
+//        .deleteCookies("JSESSIONID")
+//        .invalidateHttpSession(true)
+//        .permitAll();
+//  }
+//}
 //  databaseとの関連付け※データベース後に整理
 //  @Autowired
 //  public void configure(AuthenticationManagerBuilder auth) throws Exception{
--- api/src/main/java/com/lifegraph/team20/controllers/AuthController.java
+++ api/src/main/java/com/lifegraph/team20/controllers/AuthController.java
@@ -1,90 +1,113 @@
-//package com.lifegraph.team20.controllers;
-//
-//import javax.validation.Valid;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.security.authentication.AuthenticationManager;
-//import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-//import org.springframework.security.core.Authentication;
-//import org.springframework.security.core.context.SecurityContextHolder;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.web.bind.annotation.CrossOrigin;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestMethod;
-//import org.springframework.web.bind.annotation.RestController;
-//
-////モダンブラウザでは別のサイトにアクセスしようとすると、
-////アクセス許可の確認をするようになっている。
-////これはサイトをまたいだアクセス、Cross-Origin Resource Sharing (CORS)と言う.
-////その対策にCrossOriginアノテーションをつける
-//@CrossOrigin(origins = "*", maxAge = 3600)
-////RestControllerではViewに遷移せず、メソッドの戻り値がそのままレスポンスコンテンツになる。
-//@RestController
-//@RequestMapping("/api/auth")
-//public class AuthController {
-//  @Autowired
-//  AuthenticationManager authenticationManager;
-//
-//  @Autowired
-//  UserRepository userRepository;
-//
-//  @Autowired
-//  RoleRepositoiry roleRepository;
-//
-//  @Autowired
-//  PasswordEncoder encoder;
-//
-//  @Autowired
-//  JwtUtils jwtUtils;
-//
-//  @PostMapping("/signin")
-//        public ResponseEntity<?> authenticationUser(@Valid @RequestBody LoginRequest loginRequest){
-//
-//               Authentication authentication = authenticationManager.authenticate(
-//                               new UsernamePasswordAuthenticationToken(loginRequest.getUsername(),loginRequest.getPassword());
-//
-//                               SecurityContextHolder.getContext().setAuthentication(authentication);
-//                               String jwt = jwtUtils.generateJwtToken(authentication);
-//
-//                               UserDetailImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
-//                               List<String>roles = userDetails.getAuthorities().stream()
-//                                   .map(item -> item,getAuthority())
-//                                   .collect(collectors.toList());
-//
-//                               return ResponseEntity.ok(new JwtResponse(jwt,
-//                                   userDetails.getId(),
-//                                   userDetails.getUsername(),
-//                                   userDetails.getEmail(),
-//                                   roles));
-//        }
-//
-//  @PostMapping("/signup")
-//  public ResponseEntity<?> registerUser(@Vailid @RequestBody SignupRequest signUpRequest){
-//          if (userRepository.existsByUsername(signUpRequest.getUsername())) {
-//            return ResponseEntity
-//                   .badRequest()
-//                   body(new MessageResponse("Error: Username is alredy taken!"));
-//          }
-//
-//          if (userRepository.existsByUsername(signUpRequest.getUsername())) {
-//            return ResponseEntity
-//                    .badRequest()
-//                    .body(new MessageResponse("Error: Username is already in use!"));
-//          }
-//
-//// ユーザーアカウント登録
-//          User user = new User(signUpRequest.getUsername(),
-//              signUpRequest.getEmail(),
-//              encoder.encode(signUpRequest.getPassword()));
-//
-//          Set<>
-//  }
-//
-//  @RequestMapping(value = "/signin", method = RequestMethod.GET)
-//  public ResponseEntity<String> login() {
-//    return ResponseEntity.ok("OK");
-//  }
-//}
+package com.lifegraph.team20.controllers;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.BeanDefinitionDsl.Role;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+//モダンブラウザでは別のサイトにアクセスしようとすると、
+//アクセス許可の確認をするようになっている。
+//これはサイトをまたいだアクセス、Cross-Origin Resource Sharing (CORS)と言う.
+//その対策にCrossOriginアノテーションをつける
+@CrossOrigin(origins = "*", maxAge = 3600)
+//RestControllerではViewに遷移せず、メソッドの戻り値がそのままレスポンスコンテンツになる。
+@RestController
+@RequestMapping("/api/auth")
+public class AuthController {
+  @Autowired
+  AuthenticationManager authenticationManager;
+
+  @Autowired
+  UserRepository userRepository;
+
+  @Autowired
+  RoleRepositoiry roleRepository;
+
+  @Autowired
+  PasswordEncoder encoder;
+
+  @Autowired
+  JwtUtils jwtUtils;
+
+  @PostMapping("/signin")
+        public ResponseEntity<?> authenticationUser(@Valid @RequestBody LoginRequest loginRequest){
+
+               Authentication authentication = authenticationManager.authenticate(
+                               new UsernamePasswordAuthenticationToken(loginRequest.getUsername(),loginRequest.getPassword());
+
+                               SecurityContextHolder.getContext().setAuthentication(authentication);
+                               String jwt = jwtUtils.generateJwtToken(authentication);
+
+                               UserDetailImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+                               List<String>roles = userDetails.getAuthorities().stream()
+                                   .map(item -> item,getAuthority())
+                                   .collect(collectors.toList());
+
+                               return ResponseEntity.ok(new JwtResponse(jwt,
+                                   userDetails.getId(),
+                                   userDetails.getUsername(),
+                                   userDetails.getEmail(),
+                                   roles));
+        }
+
+  @PostMapping("/signup")
+  public ResponseEntity<?> registerUser(@Valid @RequestBody SignupRequest signUpRequest){
+          if (userRepository.existsByUsername(signUpRequest.getUsername())) {
+            return ResponseEntity
+                   .badRequest()
+                   .body(new MessageResponse("Error: Username is alredy taken!"));
+          }
+
+          if (userRepository.existsByUsername(signUpRequest.getUsername())) {
+            return ResponseEntity
+                    .badRequest()
+                    .body(new MessageResponse("Error: Username is already in use!"));
+          }
+
+// ユーザーアカウント登録
+          User user = new User(signUpRequest.getUsername(),
+              signUpRequest.getEmail(),
+              encoder.encode(signUpRequest.getPassword()));
+
+          Set<String> strRole = signUpRequest.getRole();
+          Set<Role> roles = new HashSet<>();
+
+          if (strRoles == null) {
+            Role userRole = roleRepository.findByName(ERole.ROLE_USER)
+                .orElseThrow(()-> new RuntimeException("Error: Role is not found."));
+            roles.add(userRole);
+          } else {
+            strRoles.forEach(
+
+  role -> {switch(role){case"admin":
+
+  Role adminRole = roleRepository.findByName(ERole.ROLE_ADMIN)
+      .orElseThrow(() -> new RuntimeException("Error:Role is not found."));roles.add(adminRole);
+
+  break;case"mod":
+  Role modRole = roleRepository.findByName(ERole.ROLE_MODERATOR)
+      .orElseThrow(() -> new RuntimeException("Error: Role is not found."));roles.add(modRole);
+
+  break;default:
+  Role userRole = roleRepository.findByName(ERole.ROLE_USER)
+      .orElseThrow(() -> new RuntimeException("Error: Role is not found."));roles.add(userRole);
+}});}
+
+user.setRoles(roles);userRepository.save(user);
+
+return ResponseEntity.ok(new MessageResponse("User registerd successfully!"));
