8eab0262c137be270b57c02976791c2f1e2989bb
yuto.hatano <yuto.hatano@seattleconsulting.co.jp>
2020-05-16 16:45:51 +0900

    mod:loginAPI

--- api/src/main/java/com/lifegraph/team20/controllers/AuthController.java
+++ api/src/main/java/com/lifegraph/team20/controllers/AuthController.java
@@ -34,7 +34,7 @@ import com.lifegraph.team20.security.services.UserDetailsImpl;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
-@RequestMapping("/api/auth")
+@RequestMapping("/auth")
 public class AuthController {
   @Autowired
   AuthenticationManager authenticationManager;
