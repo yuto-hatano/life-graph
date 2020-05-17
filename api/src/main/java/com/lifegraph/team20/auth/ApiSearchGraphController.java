c23b9ccf79c538cb82877fdd3b1bf1aed44e71db
haru-sato <seattle-sato@gmail.com>
2020-05-17 20:14:24 +0900

    検索API

--- api/src/main/java/com/lifegraph/team20/auth/ApiSearchGraphController.java
+++ api/src/main/java/com/lifegraph/team20/auth/ApiSearchGraphController.java
@@ -1,52 +1,49 @@
-//package com.lifegraph.team20.auth;
-//
-//import java.sql.ResultSet;
-//import java.sql.SQLException;
-//import java.util.ArrayList;
-//import java.util.HashMap;
-//import java.util.List;
-//import java.util.Map;
-//import java.util.stream.Collectors;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.jdbc.core.JdbcTemplate;
-//import org.springframework.jdbc.core.RowMapper;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RequestMethod;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@RestController
-//public class ApiSearchGraphController {
-//
-//	@RequestMapping(value = "/search", method = RequestMethod.GET)
-////	メソッドや処理を関連づけるアノテーション
-//	 public List<Map<String, Object>> search() {
-//		List<SearchGraph> SearchGraphs = selectSearchGraphs();
-////		l.40らへんのselectSearchGraphsを呼び出す
-//		List<Map<String, Object>> SearchGraphList = uploadSearchGraph(SearchGraphs);
-////		SearchGraphの値をもらったらl.57を呼び出す
-//		return SearchGraphList;
-//	}
-//	@Autowired
-//	//MySQLのデータを持ってくるライブラリ
-//	private JdbcTemplate jdbcTemplate;
-//
-//	public List<SearchGraph> selectSearchGraphs() {
-//		final String sql = "select * from parent_graphs";
-////		sqlに"select * from parent_graphs"という文字列をいれる
-//		return jdbcTemplate.query(sql, new RowMapper<SearchGraph>() {
-////			quelyの操作
-////			RowMapper:JdbcTemplate.queryの処理を実行した際に、DBから取得した結果とJavaのオブジェクトとを紐づける
-//			// SearchGraph.javaの中にそれぞれのデータを入れている　
-//			//その後にRowMapper<SearchGraph>に返却される
-//			public SearchGraph mapRow(ResultSet rs, int rowNum) throws SQLException {
-//				return new SearchGraph(rs.getInt("id"));
-////				取得したidをl.44のSearchGraphに返す
-////				さらにそのSearchGraphをl.30のselectSearchGraphに返す。
-//			}
-//		});
-//	}
-//
+package com.lifegraph.team20.auth;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApiSearchGraphController {
+
+	@RequestMapping(value = "/search", method = RequestMethod.GET)
+//	メソッドや処理を関連づけるアノテーション
+	public List<SearchGraph>searchGraph(){
+		List<SearchGraph> SearchGraphs = selectSearchGraph();
+//		l.33のselectSearchGraphsを呼び出す
+		return SearchGraphs;
+	}
+
+	@Autowired
+	//MySQLのデータを持ってくるライブラリ
+	private JdbcTemplate jdbcTemplate;
+
+	private List<SearchGraph> selectSearchGraph(){
+		final String sql = "select name,user_id,updated_at from users inner join parent_graphs on users.id = parent_graphs.user_id";
+//		sqlに"select ~"という文字列をいれる
+		return jdbcTemplate.query(sql, new RowMapper<SearchGraph>() {
+//			quelyの操作
+//			RowMapper:JdbcTemplate.queryの処理を実行した際に、DBから取得した結果とJavaのオブジェクトとを紐づける
+//			SearchGraph.javaの中にそれぞれのデータを入れている　
+//			その後にRowMapper<SearchGraph>に返却される
+				public SearchGraph mapRow(ResultSet rs, int rowNum) throws SQLException{
+					return new SearchGraph(rs.getString("name"), rs.getInt("user_id"),rs.getTimestamp("updated_at"));
+//					取得したidをl.33のSearchGraphに返す
+//					さらにそのSearchGraphをl.24のselectSearchGraphに返す。
+				}
+		});
+	}
+
+}
+
 //	public List<Map<String, Object>> uploadSearchGraph(List<SearchGraph> SearchGraphs) {
 //		List<Map<String, Object>> dataList = new ArrayList<Map<String, Object>>();
 //	    Map<String, Object> map;
@@ -64,5 +61,4 @@
 //	    }
 //		return dataList;
 //	}
-//
-//}
+
--- api/src/main/java/com/lifegraph/team20/auth/SearchGraph.java
+++ api/src/main/java/com/lifegraph/team20/auth/SearchGraph.java
@@ -1,18 +1,44 @@
 package com.lifegraph.team20.auth;
 
-public class SearchGraph {
-	private int id;
+import java.sql.Timestamp;
 
-	public SearchGraph(int id) {
-		this.setId(id);
+public class SearchGraph {
+	private String name;
+	private int user_id;
+	private Timestamp updated_at;
+
+	public SearchGraph(String string, int int1, Timestamp timestamp) {
+		// TODO Timestampの型定義ができんで自動生成したからいらんかも
+	}
+
+	public void Sample(String name, int user_id ,Timestamp updated_at) {
+		this.setName(name);
+		this.setId(user_id);
+		this.setAt(updated_at);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
 	}
 
 	public int getId() {
-		return id;
+		return user_id;
 	}
 
-	public void setId(int id) {
-		this.id = id;
+	public void setId(int Id) {
+		this.user_id = user_id;
+	}
+
+	public Timestamp getAt() {
+		return updated_at;
+	}
+
+	public void setAt(Timestamp updated_at) {
+		this.updated_at = updated_at;
 	}
 
 }
