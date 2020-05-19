f77cf66871918be189225705b9e65f8bedf88b7e
haru-sato <seattle-sato@gmail.com>
2020-05-19 12:17:24 +0900

    参照API編集

--- api/src/main/java/com/lifegraph/team20/controllers/RefApiController.java
+++ api/src/main/java/com/lifegraph/team20/controllers/RefApiController.java
@@ -22,7 +22,8 @@ import com.lifegraph.team20.models.Parent;
 public class RefApiController {
 
 
-	private static final int ParentId = 2;
+//	private static final int ParentId = 2;
+	private static final int ID = 3;
 
 	@RequestMapping(value = "/auth/ref", method = RequestMethod.GET)
 	 public List<Map<String, Object>> ref() {
@@ -37,7 +38,7 @@ public class RefApiController {
 	private JdbcTemplate jdbcTemplate;
 
 	public List<Child> selectChilds() {
-		final String sql = "select * from `child_graphs` where parent_id="+ParentId+";";
+		final String sql = "select * from `child_graphs` ";
 
 		return jdbcTemplate.query(sql, new RowMapper<Child>() {
 			// Sampleの中にそれぞれのデータを入れている　
@@ -51,11 +52,10 @@ public class RefApiController {
 	//親グラフのテーブルを丸々持ってくる
 	//Parentの中にデータを入れて、RowMapper<Parent>に返却
 	public List<Parent> selectParents(){
-		final String sql = "select * from parent_graphs";
+		final String sql = "select id from parent_graphs where id="+ID+"";
 		return jdbcTemplate.query(sql, new RowMapper<Parent>() {
 			public Parent mapRow(ResultSet rs, int rowNum) throws SQLException{
-				return new Parent(rs.getLong("id"), rs.getLong("user_id"),
-						rs.getTimestamp("created_at"), rs.getTimestamp("updated_at"));
+				return new Parent(rs.getLong("id"));
 			}
 		});
 	}
@@ -67,17 +67,9 @@ public class RefApiController {
 	    for (Parent graph: graphs) {
 	    	map = new HashMap<>();
 	    	map.put("id", graph.getId());
-	    	map.put("user_id", graph.getUserId());
-	    	map.put("created_at", graph.getCreatedAt());
-	    	map.put("update_at", graph.getUpdatedAt());
 	    	map.put("id", data.stream()
 	    			.filter(s -> s.getParentId()==graph.getId())
 	    			.collect(Collectors.toList()));
-//	      map = new HashMap<>();
-//	      map.put("name", entry.getValue());
-//	      map.put("name", entry.getKey());
-//	      map.put("data", entry.getValue());
-
 	    	dataList.add(map);
 	    }
 		return dataList;
