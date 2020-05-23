b955d863c7a12dc2dcf27757ab6a8deaa2e06c6f
yutoishida <yuto.ishida@seattleconsulting.co.jp>
2020-05-24 01:49:41 +0900

    登録画面つなぎこみ20200524

--- vue/src/store/modules/clear.js
+++ vue/src/store/modules/clear.js
@@ -13,9 +13,7 @@ export default {
   // APIによるdataの受け渡し
   actions: {
     create ({ commit }, data) {
-      debugger
       const url = '/api/auth/life_graphs'
-      debugger
       Axios.delete(url, { data })
         .then(res => commit('create', res.data))
         .catch(err => err)
