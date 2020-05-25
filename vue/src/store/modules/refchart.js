67f7de392516b15d7721b8afae80c45ca9ed0ff9
yutoishida <yuto.ishida@seattleconsulting.co.jp>
2020-05-25 15:36:24 +0900

    登録つなぎ込み修正

--- vue/src/store/modules/refchart.js
+++ vue/src/store/modules/refchart.js
@@ -12,8 +12,9 @@ export default {
   },
   // APIによるdataの受け渡し
   actions: {
-    create ({ commit }, parentId) {
-      const url = '/api/auth/ref/' + parentId
+    create ({ commit }, userId) {
+      const url = '/api/auth/ref/' + userId
+      debugger
       Axios.get(url)
         .then(res => commit('create', res.data))
         .catch(err => err)
