0e3ddab40215b6a31628be50081f9a97fc030ca9
サイトウハルカ <saitoharuka@saitouharukanoMacBook-Pro.local>
2020-05-25 17:15:17 +0900

    修正

--- vue/src/store/modules/register.js
+++ vue/src/store/modules/register.js
@@ -10,7 +10,6 @@ export default {
   // APIによるdataの受け渡し
   actions: {
     register ({ commit }, data) {
-      debugger
       const url = '/api/auth/life_graphs'
       Axios.post(url, data)
     }
