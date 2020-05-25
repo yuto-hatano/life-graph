dd0994e1ea64ef19a6f422ea8af31433d667be7d
yuto.hatano <yuto.hatano@seattleconsulting.co.jp>
2020-05-25 09:35:55 +0900

    mod:connectRef

--- api/src/main/java/com/lifegraph/team20/controllers/RefApiController.java
+++ api/src/main/java/com/lifegraph/team20/controllers/RefApiController.java
@@ -12,25 +12,28 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.lifegraph.team20.models.Child;
+import com.lifegraph.team20.models.ChildGraph;
 import com.lifegraph.team20.models.Parent;
 
 @RestController
 public class RefApiController {
 
-  @GetMapping(value = "/auth/ref/{parentId}")
-  //  @GetMapping(value = "/auth/ref/{parent_id}")
-  public ResponseEntity<List<Child>> Child(
+  //  @GetMapping(value = "/ref/{parentId}")
+  @GetMapping(value = "/auth/ref/{id}")
+  public ResponseEntity<ChildGraph> Child(
       //      @RequestBody Child data) {
-      @PathVariable("parentId") long parentId) {
+      @PathVariable("id") Integer id) {
     //	Optional:その値がnullかもしれないことを表現するクラス
     //			URLが叩かれたときにこれが動く
     //    long parentId = data.getParentId();
-    List<Child> Child = selectChilds(parentId);
+    ChildGraph Child = selectChilds(id);
     //			l.29のselectSearchGraphを呼び出す
     return ResponseEntity.ok(Child);
   }
@@ -48,18 +51,29 @@ public class RefApiController {
   //MySQLのデータを持ってくるライブラリ
   private JdbcTemplate jdbcTemplate;
 
-  public List<Child> selectChilds(long ID) {
-    final String sql = "select * from child_graphs where parent_id=" + ID + "";
-
-    return jdbcTemplate.query(sql, new RowMapper<Child>() {
-      // Sampleの中にそれぞれのデータを入れている　
-      //その後にRowMapper<Sample>に返却される
-      public Child mapRow(ResultSet rs, int rowNum) throws SQLException {
-        return new Child(rs.getInt("id"), rs.getInt("parent_id"),
-            rs.getInt("age"), rs.getInt("score"), rs.getString("comment"));
+  public ChildGraph selectChilds(long ID) {
+    final String sql = "select child_graphs.id,parent_id,age,score,comment from child_graphs inner join parent_graphs on child_graphs.parent_id = parent_graphs.id\n"
+        +
+        "inner join users on parent_graphs.user_id = users.id where users.id= :id;";
+    SqlParameterSource param = new MapSqlParameterSource().addValue("id", ID);
+    ChildGraph result = (ChildGraph) jdbcTemplate.queryForList(sql, param, new RowMapper<ChildGraph>() {
+      public ChildGraph mapRow(ResultSet rs, int rowNum) throws SQLException {
+        return new ChildGraph(rs.getInt("id"), rs.getInt("parent_id"), rs.getInt("age"), rs.getInt("score"),
+            rs.getString("name"));
       }
     });
+    return result;
   }
+
+  //    return jdbcTemplate.query(sql, new RowMapper<Child>() {
+  //      // Sampleの中にそれぞれのデータを入れている　
+  //      //その後にRowMapper<Sample>に返却される
+  //      public Child mapRow(ResultSet rs, int rowNum) throws SQLException {
+  //        return new Child(rs.getInt("id"), rs.getInt("parent_id"),
+  //            rs.getInt("age"), rs.getInt("score"), rs.getString("comment"));
+  //      }
+  //    });
+
   //親グラフのテーブルを丸々持ってくる
   //Parentの中にデータを入れて、RowMapper<Parent>に返却
   //	public List<User> selectUsers(int ID){
@@ -88,7 +102,9 @@ public class RefApiController {
       map = new HashMap<>();
       map.put("id", graph.getId());
       map.put("id", data.stream()
-          .filter(s -> s.getParentId() == graph.getId())
+          .filter(
+
+              s -> s.getParentId() == graph.getId())
           .collect(Collectors.toList()));
       dataList.add(map);
     }
--- api/src/main/java/com/lifegraph/team20/models/ChildGraph.java
+++ api/src/main/java/com/lifegraph/team20/models/ChildGraph.java
@@ -3,6 +3,7 @@ package com.lifegraph.team20.models;
 import lombok.Data;
 
 @Data
+
 public class ChildGraph {
 
   private long id;
@@ -11,4 +12,11 @@ public class ChildGraph {
   private int score;
   private String comment;
 
+  public ChildGraph(long id, long parentId, int age, int score, String comment) {
+    this.id = id;
+    this.parentId = parentId;
+    this.age = age;
+    this.score = score;
+    this.comment = comment;
+  }
 }
